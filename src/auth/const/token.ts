76aebf4209ce619916062d35124cb4ba32cf180f
Tnalxmsk <alstnwkd990@naver.com>
2025-11-10 00:41:53 +0900

    :sparkles: feat: Token 관련 상수화

---
+++ src/auth/const/token.ts
@@ -0,0 +1,5 @@
+export const REFRESH_TOKEN = 'refresh';
+export const ACCESS_TOKEN = 'access';
+export const JWT_SECRET = 'JWT_SECRET';
+export const REFRESH_TOKEN_EXPIRED = 'REFRESH_TOKEN_EXPIRED';
+export const ACCESS_TOKEN_EXPIRED = 'ACCESS_TOKEN_EXPIRED';
