e8c75ce4ffd30f4ded0272e1aeabde8007a97321
Tnalxmsk <alstnwkd990@naver.com>
2025-12-01 04:30:38 +0900

    :sparkles: feat: CI/CD 테스트
    구축했던 CI/CD 동작 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { IsPublic } from './common/decorator/is-public.decorator';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { IsPublic } from './common/decorator/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
   @IsPublic()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Get('test')
   @ApiBearerAuth('authorization')
   test(): string {
