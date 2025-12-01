f4e6a1362574eb88bce30ce4c518fef5002776d9
Tnalxmsk <alstnwkd990@naver.com>
2025-12-02 01:09:06 +0900

    :sparkles: feat: relations 속성 추가

--- src/ramps/const/default-post-find-options.const.ts
+++ src/ramps/const/default-post-find-options.const.ts
@@ -2,5 +2,7 @@ import { FindManyOptions } from 'typeorm';
 import { RampsModel } from '../entities/ramps.entity';
 
 export const DEFAULT_RAMPS_FIND_OPTIONS: FindManyOptions<RampsModel> = {
-  relations: {},
+  relations: {
+    images: true,
+  },
 };
