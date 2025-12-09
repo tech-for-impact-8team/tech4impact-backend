3b070995b0ae769eddd02c8c7c4e7d60790a2462
Tnalxmsk <alstnwkd990@naver.com>
2025-12-09 17:09:24 +0900

    :sparkles: feat: cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://localhost:3000',
-      'https://tech4impact-front.vercel.app/',
+      'https://tech4impact-front.vercel.app',
     ],
     credentials: true,
   });
